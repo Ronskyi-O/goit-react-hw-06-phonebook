c5fd90549677b520883b5e7619036bbde71f67ae
Oleksandr Ronskyi <oleksandr.ronskyi@gmail.com>
2023-01-25 01:19:42 -0500

    fixed bug in ContactsFilter

--- src/components/ContactsFilter/ContactsFilter.jsx
+++ src/components/ContactsFilter/ContactsFilter.jsx
@@ -1,10 +1,10 @@
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { setFilter } from 'redux/contacts.slice'
-
 
 import { FilterLabel } from './ContactsFilter.styled'
 
 export const ContactsFilter = () => {
+    const filter = useSelector((state) => state.contacts.filter)
     const dispatch = useDispatch()
 
     const onFilterChange = filter => {
@@ -15,6 +15,7 @@ export const ContactsFilter = () => {
         <FilterLabel>Find contacts by name
             <input
                 type="text"
+                value={filter}
                 onChange={(event) => onFilterChange(event.currentTarget.value)}
             />
         </FilterLabel>
